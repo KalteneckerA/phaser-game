1fdd23c33f253c6c1297b8d8e24c819e6a190864
KalteneckerA <150678281+KalteneckerA@users.noreply.github.com>
2024-03-22 09:50:00 +0100

    Scene1.js

--- Scene1.js
+++ Scene1.js
@@ -3,10 +3,10 @@ class Scene1 extends Phaser.Scene {
         super("bootGame");
     }
     preload() {
-        this.add.image("background", "assets/background.png");
-        this.add.image("ship","assets/ship.png");
-        this.add.image("ship2","assets/ship2.png");
-        this.add.image("ship3","assets/ship3.png");
+        this.add.image("background", "background.png");
+        this.add.image("ship","ship.png");
+        this.add.image("ship2","ship2.png");
+        this.add.image("ship3","ship3.png");
     }
         create() {
         this.add.text(20, 20, "Loading game...");
